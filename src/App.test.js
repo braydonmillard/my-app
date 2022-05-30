b68976b4b6b5918ee72668d25f2fa3364d9503f2
braydonmillard <46946203+braydonmillard@users.noreply.github.com>
2022-05-30 13:06:19 -0400

    Added test to check for correct link

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement.href).toContain('ultimateqa.com');
+})
