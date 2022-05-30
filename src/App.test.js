3dcc5483d336f42a4cbb00b7b3ce49f9ef71bcff
braydonmillard <46946203+braydonmillard@users.noreply.github.com>
2022-05-30 13:36:17 -0400

    Modified test and ci target branch

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { getByTestId, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link');
   expect(linkElement).toBeInTheDocument();
 });
 
